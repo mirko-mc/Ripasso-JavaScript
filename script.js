2dce764214e7d8d417a3ca023d7b93e5b2147bee
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-05-21 22:01:12 +0200

    base exercise and extra exercise 1 completed

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 // 1. Crea una funzione che controlli due numeri interi. Ritorna "true" se uno dei due è 50 o se la somma dei due è 50.
+function controllaCinquanta(n1, n2) {
+  return n1 === 50 || n2 === 50 || n1 + n2 === 50;
+}
+console.log(`ESERCIZIO 1: ${controllaCinquanta(50, 50)}`);
 
 // 2. Crea una funzione che rimuova il carattere ad una specifica posizione da una stringa. Passa la stringa e la posizione come parametri e ritorna la stringa modificata.
+function rimuoviCarattere(testo, posizione) {
+  return testo.slice(0, posizione - 1) + testo.slice(posizione - 1 + 1);
+}
+console.log(`ESERCIZIO 2: ${rimuoviCarattere("EPICODE", 2)}`);
 
 // 3. Crea una funzione che controlli se due numeri siano compresi tra 40 e 60 o tra 70 e 100. Ritorna "true" se rispecchiano queste condizioni, altrimenti ritorna "false".
+function compresi(n) {
+  return (n > 39 && n < 61) || (n > 69 && n < 101);
+}
+console.log(`ESERCIZIO 3: 39 => ${compresi(39)}`);
+console.log(`ESERCIZIO 3: 40 => ${compresi(40)}`);
+console.log(`ESERCIZIO 3: 60 => ${compresi(60)}`);
+console.log(`ESERCIZIO 3: 61 => ${compresi(61)}`);
+console.log(`ESERCIZIO 3: 69 => ${compresi(69)}`);
+console.log(`ESERCIZIO 3: 70 => ${compresi(70)}`);
+console.log(`ESERCIZIO 3: 100 => ${compresi(100)}`);
+console.log(`ESERCIZIO 3: 101 => ${compresi(101)}`);
 
 // 4. Crea una funzione che accetti un nome di una città come parametro e ritorni il nome stesso se inizia con "Los" o "New", altrimenti "false".
+function citta(nome) {
+  if (
+    nome.toLowerCase().startsWith("los") ||
+    nome.toLowerCase().startsWith("new")
+  )
+    return nome;
+  return false;
+}
+console.log(`ESERCIZIO 4: Los Santos => ${citta("Los Santos")}`);
+console.log(`ESERCIZIO 4: New York => ${citta("New York")}`);
+console.log(`ESERCIZIO 4: Vice City => ${citta("Vice City")}`);
 
 // 5. Crea una funzione che calcoli e ritorni la somma di tutti gli elementi di un array. L'array deve essere passato come parametro.
+function sommaElementi(a) {
+  let somma = 0;
+  a.forEach((elemento) => {
+    somma += elemento;
+  });
+  return somma;
+}
+console.log(`ESERCIZIO 5: ${sommaElementi([5, 7, 9, 1, 5, 3, 8])}`);
 
 // 6. Crea una funzione che controlli che un array NON contenga i numeri 1 o 3. Se NON li contiene, ritorna "true", altrimenti ritorna "false".
+function nonContiene(n) {
+  if (n.indexOf(1) < 0 && n.indexOf(3) < 0) return true;
+  else return false;
+}
+console.log(`ESERCIZIO 6: 1,3,5,7 => ${nonContiene([1, 3, 5, 7])}`);
+console.log(`ESERCIZIO 6: 2,4,6,8 => ${nonContiene([2, 4, 6, 8])}`);
 
 /*
     7. Crea una funzione per trovare il tipo di un angolo i cui gradi sono passati come parametro.
@@ -17,11 +61,48 @@
         Angolo retto: 90° => ritorna "retto"
         Angolo piatto: 180° => ritorna "piatto"
 */
+function angoli(gradi) {
+  if (gradi < 90) return "Angolo Acuto";
+  else if (gradi === 90) return "Angolo Retto";
+  else if (gradi === 180) return "Angolo Piatto";
+  else if (gradi > 90 && gradi < 180) return "Angolo Ottuso";
+}
+console.log(`ESERCIZIO 7: 90 => ${angoli(90)}`);
+console.log(`ESERCIZIO 7: 180 => ${angoli(180)}`);
+console.log(`ESERCIZIO 7: 89 => ${angoli(89)}`);
+console.log(`ESERCIZIO 7: 91 => ${angoli(91)}`);
 
 // 8. Crea una funzione che crei un acronimo a partire da una frase. Es. "Fabbrica Italiana Automobili Torino" => deve ritornare "FIAT".
+function acronimo(frase) {
+  let acronimo = "";
+  let parole = frase.split(" ");
+  parole.forEach((parola) => {
+    acronimo += parola.slice(0, 1);
+  });
+  return acronimo.toUpperCase();
+}
+console.log(`ESERCIZIO 8: ${acronimo("fabbrica italiana automobili torino")}`);
 
 // NOTA: tutti gli esercizi devono essere svolti usando le funzioni
 // 1. Partendo da una stringa (passata come parametro), ritorna il carattere più usato nella stringa stessa.
+function piuUsato(testo) {
+  let caratteri = Array.from(testo.toLowerCase());
+  caratteri.sort();
+  let occorrenze = 0;
+  let maxOccorrenze = ["", 0];
+  for (let i = 0; i < caratteri.length; i++) {
+    for (let j = 0; j < caratteri.length; j++) {
+      if (j === caratteri.length - 1) occorrenze = 0;
+      if (caratteri[i] === caratteri[j]) occorrenze++;
+      if (occorrenze > maxOccorrenze[1]) {
+        maxOccorrenze[0] = caratteri[i];
+        maxOccorrenze[1] = occorrenze;
+      }
+    }
+  }
+  return `${maxOccorrenze[0]} con ${maxOccorrenze[1]} occorrenze`;
+}
+console.log(`ESERCIZIO 9: EPICODE => ${piuUsato("EPICODE")}`);
 
 // 2. Controlla che due stringhe passate come parametri siano gli anagrammi l’una dell’altra. Ignora punteggiatura e spazi e ricordate di rendere la stringa tutta in minuscolo. Se le due parole sono anagrammi, ritorna `true`, altrimenti ritorna `false`.
 
